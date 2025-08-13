8e9ee82296f89494563010bda827b0701b3549ca
Hoa Pham <hoaqb208@gmail.com>
2025-08-13 17:04:37 +0700

    Add Ribbon and SearchSet API classes
    Introduces RibbonAPI for creating ribbon tabs and SearchSetCreator for generating search sets in Navisworks. Also updates the project file to include a new _Viewpoint folder.

---
+++ Libs.NavisworksAPI/Libs.NavisworksAPI/_Ribbon/RibbonAPI.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Windows;
+
+namespace Libs.NavisworksAPI._Ribbon
+{
+    public class RibbonAPI
+    {
+        public static RibbonTab CreateRibbon(string name)
+        {
+            // Lấy control ribbon chính
+            var ribbon = ComponentManager.Ribbon;
+            // Tạo tab mới nếu chưa có
+            var newTab = ribbon.FindTab(name);
+            if (newTab == null)
+            {
+                newTab = new RibbonTab { Id = name, Title = name };
+                ribbon.Tabs.Add(newTab);
+            }
+            return newTab;
+        }
+    }
+}
---
+++ Libs.NavisworksAPI/Libs.NavisworksAPI/_SearchSet/SearchSetCreator.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Navisworks.Api;
+
+namespace Libs.NavisworksAPI._SearchSet
+{
+    public class SearchSetCreator
+    {
+        public static void CreateSearchSet(string setName, string category, string propertyName, string conditionName, string value)
+        {
+            Document doc = Autodesk.Navisworks.Api.Application.ActiveDocument;
+
+            // Tạo Search
+            Search search = new Search();
+            search.Selection.SelectAll();
+
+            SearchCondition condition = SearchCondition
+                .HasPropertyByDisplayName("Element", "Category")
+                .DisplayStringContains("Walls");
+
+            search.SearchConditions.Add(condition);
+
+            // Tạo Search Set
+            SelectionSet searchSet = new SelectionSet(search)
+            {
+                DisplayName = "My Wall Search Set"
+            };
+
+
+
+
+
+          
+
+        }
+    }
+}
