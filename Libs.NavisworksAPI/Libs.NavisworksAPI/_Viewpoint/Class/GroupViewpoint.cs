2db233447d950c736d753b4d0fadcb1e1eaa07cb
Hoa Pham <hoaqb208@gmail.com>
2025-08-27 16:55:03 +0700

    Add viewpoint grouping and utility classes
    Introduces GroupViewpoint class for hierarchical grouping of viewpoints and ViewpointUtils for retrieving and organizing SavedViewpoints from the Navisworks document. Removes unused folder reference from the project file.

---
+++ Libs.NavisworksAPI/Libs.NavisworksAPI/_Viewpoint/Class/GroupViewpoint.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Navisworks.Api;
+using System.Collections.Generic;
+
+namespace Libs.NavisworksAPI._Viewpoint.Class
+{
+    public class GroupViewpoint
+    {
+        public string Name { get; }
+        public GroupItem Original { get; }
+        public List<GroupViewpoint> Groups { get; set; } = new List<GroupViewpoint>();
+        public List<SavedViewpoint> Viewpoints { get; set; } = new List<SavedViewpoint>();
+
+        public GroupViewpoint(string name, GroupItem original)
+        {
+            Name = name;
+            Original = original;
+        }
+    }
+}
---
+++ Libs.NavisworksAPI/Libs.NavisworksAPI/_Viewpoint/ViewpointUtils.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.Navisworks.Api;
+using Libs.NavisworksAPI._Viewpoint.Class;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Libs.NavisworksAPI._Viewpoint
+{
+    public class ViewpointUtils
+    {
+        public static List<SavedViewpoint> GetAllViewpoints(GroupViewpoint root)
+        {
+            if (root == null) return new List<SavedViewpoint>();
+            return root.Viewpoints.Concat(root.Groups.SelectMany(g => GetAllViewpoints(g))).ToList();
+        }
+
+        public static GroupViewpoint GetRootViewpoints()
+        {
+            var doc = Autodesk.Navisworks.Api.Application.ActiveDocument;
+            var root = new GroupViewpoint("Root", null);
+            foreach (var savedItem in doc.SavedViewpoints.Value)
+            {
+                if (savedItem is GroupItem group) root.Groups.Add(GetGroupViewpoint(group));
+                else if (savedItem is SavedViewpoint viewpoint) root.Viewpoints.Add(viewpoint);
+            }
+            return root;
+        }
+
+        private static GroupViewpoint GetGroupViewpoint(GroupItem groupItem)
+        {
+            var groupViewpoint = new GroupViewpoint(groupItem.DisplayName, groupItem);
+            foreach (var savedItem in groupItem.Children)
+            {
+                if (savedItem is GroupItem gr) groupViewpoint.Groups.Add(GetGroupViewpoint(gr));
+                else if (savedItem is SavedViewpoint vp) groupViewpoint.Viewpoints.Add(vp);
+            }
+            return groupViewpoint;
+        }
+    }
+}
